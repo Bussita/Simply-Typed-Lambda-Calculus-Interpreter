50185bd7480ac0d73acf520624683b3d9e6e09fa
Bussita <santiagobussanich321@gmail.com>
2025-11-20 19:31:50 -0300

    changes

---
+++ src/Simplytyped.hs
@@ -0,0 +1,121 @@
+module Simplytyped
+  ( conversion
+  ,    -- conversion a terminos localmente sin nombre
+    eval
+  ,          -- evaluador
+    infer
+  ,         -- inferidor de tipos
+    quote          -- valores -> terminos
+  )
+where
+
+import           Data.List
+import           Data.Maybe
+import           Prelude                 hiding ( (>>=) )
+import           Text.PrettyPrint.HughesPJ      ( render )
+import           PrettyPrinter
+import           Common
+import Common (LamTerm(LVar, LAbs))
+
+-----------------------
+-- conversion
+-----------------------
+
+-- conversion a términos localmente sin nombres
+conversion :: LamTerm -> Term
+conversion = conversionAux []
+
+conversionAux :: [String] -> LamTerm -> Term
+conversionAux xs (LApp t1 t2) = conversionAux xs t1 :@: conversionAux xs t2
+conversionAux xs (LAbs var t term) = Lam t (conversionAux (var:xs) term)
+conversionAux xs (LVar var) = case isBound var xs 0 of
+                                Just i -> Bound i
+                                Nothing -> Free (Global var)
+
+isBound :: String -> [String] -> Int -> Maybe Int
+isBound var (v:vs) i = if var == v then Just i else isBound var vs (i+1)
+isBound var [] _ = Nothing
+----------------------------
+--- evaluador de términos
+----------------------------
+
+-- substituye una variable por un término en otro término
+sub :: Int -> Term -> Term -> Term
+sub i t (Bound j) | i == j    = t
+sub _ _ (Bound j) | otherwise = Bound j
+sub _ _ (Free n   )           = Free n
+sub i t (u   :@: v)           = sub i t u :@: sub i t v
+sub i t (Lam t'  u)           = Lam t' (sub (i + 1) t u)
+
+-- convierte un valor en el término equivalente
+quote :: Value -> Term
+quote (VLam t f) = Lam t f
+
+-- evalúa un término en un entorno dado
+eval :: NameEnv Value Type -> Term -> Value
+
+eval env (Free x) =
+  case lookup x env of
+    Just (v, _) -> v
+    Nothing -> error "Variable no encontrada"
+
+eval env (Lam t body) =
+  VLam t body
+
+eval env (t1 :@: t2) =
+  case eval env t1 of
+    VLam _ body ->
+      let v2 = eval env t2
+      in eval env (sub 0 (quote v2) body)
+    _ -> error "Aplicación a un no-lambda"
+
+
+
+----------------------
+--- type checker
+-----------------------
+
+-- infiere el tipo de un término
+infer :: NameEnv Value Type -> Term -> Either String Type
+infer = infer' []
+
+-- definiciones auxiliares
+ret :: Type -> Either String Type
+ret = Right
+
+err :: String -> Either String Type
+err = Left
+
+(>>=)
+  :: Either String Type -> (Type -> Either String Type) -> Either String Type
+(>>=) v f = either Left f v
+-- fcs. de error
+
+matchError :: Type -> Type -> Either String Type
+matchError t1 t2 =
+  err
+    $  "se esperaba "
+    ++ render (printType t1)
+    ++ ", pero "
+    ++ render (printType t2)
+    ++ " fue inferido."
+
+notfunError :: Type -> Either String Type
+notfunError t1 = err $ render (printType t1) ++ " no puede ser aplicado."
+
+notfoundError :: Name -> Either String Type
+notfoundError n = err $ show n ++ " no está definida."
+
+-- infiere el tipo de un término a partir de un entorno local de variables y un entorno global
+infer' :: Context -> NameEnv Value Type -> Term -> Either String Type
+infer' c _ (Bound i) = ret (c !! i)
+infer' _ e (Free  n) = case lookup n e of
+  Nothing     -> notfoundError n
+  Just (_, t) -> ret t
+infer' c e (t :@: u) = infer' c e t >>= \tt -> infer' c e u >>= \tu ->
+  case tt of
+    FunT t1 t2 -> if (tu == t1) then ret t2 else matchError t1 tu
+    _          -> notfunError tt
+infer' c e (Lam t u) = infer' (t : c) e u >>= \tu -> ret $ FunT t tu
+
+
